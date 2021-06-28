7a2f426827bb026fc15d0fdff44a6611ba3069ff
Camsz27 <camiloagr2707@gmail.com>
2021-06-27 21:57:31 -0500

    Text changes depending on whose turn it is

--- script.js
+++ script.js
@@ -1,73 +1,4 @@
-const selectPlayer1 = document.querySelectorAll("button.player1");
-const selectPlayer2 = document.querySelectorAll("button.player2");
 const body = document.querySelector("body");
-const button = document.getElementById("try");
-
-console.log(selectPlayer1);
-console.log(selectPlayer2);
-
-selectPlayer1.forEach((button) =>
-  button.addEventListener("click", player1Select)
-);
-
-selectPlayer2.forEach((button) =>
-  button.addEventListener("click", player2Select)
-);
-
-/* button.addEventListener("click", winningMessage); */
-
-/* function winningMessage() {
-  const div = document.createElement("div");
-  const container = document.createElement("div");
-  const text = document.createElement("h2");
-  const restartButton = document.createElement("button");
-  div.classList.add("modal");
-  container.classList.add("modalContent");
-  restartButton.textContent = "Restart Game";
-  restartButton.setAttribute("id", "restartButton");
-  restartButton.addEventListener("click", displayController.create);
-  text.textContent = "Player 1 is the Winner!!!";
-  container.append(text);
-  container.append(restartButton);
-  div.append(container);
-  body.append(div);
-} */
-
-function player1Select() {
-  const branch = this.parentNode.parentNode;
-  resetButtons(branch);
-  this.classList.toggle("active");
-  return 1;
-}
-
-function player2Select() {
-  const branch = this.parentNode.parentNode;
-  resetButtons(branch);
-  this.classList.toggle("active");
-  return 1;
-}
-
-function resetButtons(branch) {
-  const playerContainer = branch.childNodes;
-  for (let i = 1; i < playerContainer.length; i++) {
-    const listItem = playerContainer[i];
-    for (let j = 0; j < listItem.childNodes.length; j++) {
-      console.log(typeof listItem.childNodes[j].classList);
-      if (
-        typeof listItem.childNodes[j].classList !== "undefined" &&
-        listItem.childNodes[j].classList.contains("active")
-      ) {
-        listItem.childNodes[j].classList.remove("active");
-      }
-    }
-  }
-}
-
-function playersSelected() {
-  if (player1Select === 1 && player2Select === 1) {
-    console.log("both have been pressed");
-  }
-}
 
 const displayController = (() => {
   const clear = () => {
@@ -98,8 +29,10 @@ const displayController = (() => {
     aiItem1.classList.add("option");
     buttonPlayer1Human.classList.add("option", "player1");
     buttonPlayer1Human.textContent = "Human";
+    buttonPlayer1Human.addEventListener("click", playerSelect);
     buttonPlayer1Ai.classList.add("option", "player1");
     buttonPlayer1Ai.textContent = "AI";
+    buttonPlayer1Ai.addEventListener("click", playerSelect);
     humanItem1.append(buttonPlayer1Human);
     aiItem1.append(buttonPlayer1Ai);
     list1.append(player1Item, humanItem1, aiItem1);
@@ -112,8 +45,10 @@ const displayController = (() => {
     aiItem2.classList.add("option");
     buttonPlayer2Human.classList.add("option", "player2");
     buttonPlayer2Human.textContent = "Human";
+    buttonPlayer2Human.addEventListener("click", playerSelect);
     buttonPlayer2Ai.classList.add("option", "player2");
     buttonPlayer2Ai.textContent = "AI";
+    buttonPlayer2Ai.addEventListener("click", playerSelect);
     humanItem2.append(buttonPlayer2Human);
     aiItem2.append(buttonPlayer2Ai);
     list2.append(player2Item, humanItem2, aiItem2);
@@ -139,7 +74,50 @@ const displayController = (() => {
     body.append(div);
   };
 
-  return { clear, create };
+  const resetButtons = (branch) => {
+    const playerContainer = branch.childNodes;
+    for (let i = 1; i < playerContainer.length; i++) {
+      const listItem = playerContainer[i];
+      for (let j = 0; j < listItem.childNodes.length; j++) {
+        if (
+          typeof listItem.childNodes[j].classList !== "undefined" &&
+          listItem.childNodes[j].classList.contains("active")
+        ) {
+          listItem.childNodes[j].classList.remove("active");
+        }
+      }
+    }
+  };
+
+  const playerSelect = (e) => {
+    const branch = e.target.parentNode.parentNode;
+    resetButtons(branch);
+    e.target.classList.toggle("active");
+    checkSelect();
+    return 1;
+  };
+
+  const checkSelect = () => {
+    const playerContainer = document.querySelector(".playerSelection");
+    const player1 = playerContainer.childNodes[0];
+    const player2 = playerContainer.childNodes[1];
+    if (
+      (player1.childNodes[1].firstChild.classList.contains("active") ||
+        player1.childNodes[2].firstChild.classList.contains("active")) &&
+      (player2.childNodes[1].firstChild.classList.contains("active") ||
+        player2.childNodes[2].firstChild.classList.contains("active"))
+    ) {
+      clear();
+      gameBoard.create();
+    }
+  };
+
+  const changeTurnText = () => {
+    const container = document.querySelector("#turnContainer");
+    container.firstChild.textContent = Player("Player1", "X").turnText();
+  };
+
+  return { clear, create, winningMessage, changeTurnText };
 })();
 
 const gameBoard = (() => {
@@ -176,3 +154,12 @@ const gameBoard = (() => {
 
   return { create, clear };
 })();
+
+const Player = (name, mark) => {
+  const turnText = () => {
+    return `${name}'s (${mark}) turn`;
+  };
+  return { turnText };
+};
+
+displayController.create();
